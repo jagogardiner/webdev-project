3043aabc16d17b43cd0e6f93eed20a385e3c4b6e
nysascape <jagogardiner@gmail.com>
2021-11-04 14:39:37 +0000

    Start adding information for hotels
    Change-Id: I424ffb0a87dff8bf943e55fc3e072548ecf6c2db
    Signed-off-by: nysascape <jagogardiner@gmail.com>

--- app/model.py
+++ app/model.py
@@ -3,13 +3,21 @@ from flask import redirect, flash, current_app, url_for
 from flask_login import UserMixin, current_user
 from functools import wraps
 
-
+# Database class for a user
 class User(db.Model, UserMixin):
     # primary keys are required by SQLAlchemy
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     passwordHash = db.Column(db.String(500))
     name = db.Column(db.String(100))
+
+# Database class for a hotel
+class Hotel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(20))
+    capacity = db.Column(db.Integer)
+    peakPrice = db.Column(db.Integer)
+    offPeakPrice = db.Column(db.Integer)
 
 
 def admin_required(f):
