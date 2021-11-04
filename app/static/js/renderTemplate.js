44af7dea84bd45c843a93f0b0609c7a07a4a38c1
nysascape <jagogardiner@gmail.com>
2021-11-04 00:22:23 +0000

    Horizon: Redo a lot of elements
    * Change header and footer design to light
    * Refactor Javascript to optimise page loading
    * Fixed header and footer with collapse for mobile layouts
    * Make layout more consistent with containers and padding
    + whatever else I've forgotten
    
    Change-Id: I34b6db35ad0a3a9c39984f93669940cfdfb04d0c
    Signed-off-by: nysascape <jagogardiner@gmail.com>

--- app/static/js/renderTemplate.js
+++ app/static/js/renderTemplate.js
@@ -1,6 +1,15 @@
+/*
+@function load()
+Function to load header and footer using jQuery.
+Uses a callback so elements inside these HTML files can be
+modified on the fly.
+*/
 function load(callback) {
+	// Load header
 	$("#header").load("/header", function () {
+		// Make sure we run callback ONLY if it is a type "function".
 		typeof callback === "function" && callback();
 	});
-	$("#footer").load("/footer");
+	// Load footer
+	$("#footer").load("/footer"); // Footer does not have a callback as we do not modify elements inside the header.
 }
