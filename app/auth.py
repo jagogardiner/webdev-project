5a8aa1aad727a548b9d1008b83fd054e07a35df0
Jago Gardiner <jagogardiner@gmail.com>
2021-12-20 13:14:21 +0000

    Slightly better working pdf with qr code * still to add information from booking≈
    Change-Id: I63a2790114b4f28538cd8bc2af83482104bcd84f
    Signed-off-by: Jago Gardiner <jagogardiner@gmail.com>

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,14 @@
-from flask import Blueprint, render_template, redirect, request, flash, current_app, url_for, abort, send_file
+from flask import (
+    Blueprint,
+    render_template,
+    redirect,
+    request,
+    flash,
+    current_app,
+    url_for,
+    abort,
+    send_file,
+)
 from flask_login import login_required, login_user, current_user, logout_user
 from sqlalchemy.orm.session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,14 +19,14 @@ from functools import wraps
 from datetime import date
 from app.pdf import Receipt
 from flask import make_response
-import os
-import base64
+import random
+import string
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
 
 def redirect_dest(fallback):
-    dest = request.args.get('next')
+    dest = request.args.get("next")
     try:
         return redirect(dest)
     except:
@@ -30,120 +40,150 @@ def admin_required(f):
             return f(*args, **kwargs)
         else:
             flash("You need to be an admin to view this page.")
-            return redirect('/')
+            return redirect("/")
+
     return wrap
 
 
-@auth.route("/login", methods=['POST', 'GET'])
+@auth.route("/login", methods=["POST", "GET"])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        remember = True if request.form.get('remember-me') else False
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        remember = True if request.form.get("remember-me") else False
 
         user = User.query.filter_by(email=email).first()
 
         if not user or not check_password_hash(user.passwordHash, password):
-            flash('Please check your login details and try again.')
-            return redirect('/login')
+            flash("Please check your login details and try again.")
+            return redirect("/login")
 
-        current_app.logger.info('Logging in')
+        current_app.logger.info("Logging in")
         login_user(user, remember=remember)
-        if(user.id == 1):
-            return redirect('/admin')
-        return redirect_dest(fallback=url_for('app.home'))
+        if user.id == 1:
+            return redirect("/admin")
+        return redirect_dest(fallback=url_for("app.home"))
 
     # or:
-    return render_template('login.html')
+    return render_template("login.html")
 
 
-@auth.route("/signup", methods=['POST', 'GET'])
+@auth.route("/signup", methods=["POST", "GET"])
 def signup():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        name = request.form.get('name')
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        name = request.form.get("name")
 
         user = User.query.filter_by(email=email).first()
         # if this returns a user, then the email already exists in database
         if user:
-            flash('Email address already exists!')
-            return redirect('/signup')
+            flash("Email address already exists!")
+            return redirect("/signup")
 
         # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-        new_user = User(email=email, passwordHash=generate_password_hash(
-            password, method='sha256'), name=name)
+        new_user = User(
+            email=email,
+            passwordHash=generate_password_hash(password, method="sha256"),
+            name=name,
+        )
 
         # add the new user to the database
         db.session.add(new_user)
         db.session.commit()
 
-        return redirect('/login')
+        return redirect("/login")
 
-    return render_template('signup.html')
+    return render_template("signup.html")
 
 
 @auth.route("/member")
 @login_required
 def member_page():
-    if(current_user.id == 1):
-        return redirect('/admin')
+    if current_user.id == 1:
+        return redirect("/admin")
 
     # Get a list of future user bookings
-    future_bookings = db.session.query(Booking).join(
-        Hotel, Hotel.id == Booking.hotel_id).filter(current_user.id == Booking.user_id).filter(Booking.start_date >= date.today()).all()
+    future_bookings = (
+        db.session.query(Booking)
+        .join(Hotel, Hotel.id == Booking.hotel_id)
+        .filter(current_user.id == Booking.user_id)
+        .filter(Booking.start_date >= date.today())
+        .all()
+    )
 
     # Get a list of expired user bookings
-    exp_bookings = db.session.query(Booking).join(
-        Hotel, Hotel.id == Booking.hotel_id).filter(current_user.id == Booking.user_id).filter(Booking.start_date < date.today()).all()
+    exp_bookings = (
+        db.session.query(Booking)
+        .join(Hotel, Hotel.id == Booking.hotel_id)
+        .filter(current_user.id == Booking.user_id)
+        .filter(Booking.start_date < date.today())
+        .all()
+    )
 
-    return render_template('member.html', user=current_user, future_bookings=future_bookings, exp_bookings=exp_bookings)
+    return render_template(
+        "member.html",
+        user=current_user,
+        future_bookings=future_bookings,
+        exp_bookings=exp_bookings,
+    )
 
 
 @auth.route("/admin")
 @login_required
 @admin_required
 def admin_dash():
-    return render_template('admin.html')
+    return render_template("admin.html")
 
 
 @auth.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@auth.route("/booking/<city>", methods=['POST', 'GET'])
+@auth.route("/booking/<city>", methods=["POST", "GET"])
 @login_required
 def booking(city):
     hotel = Hotel.query.filter_by(city=city).first_or_404()
-    if request.method == 'POST':
-        roomType = request.form.get('roomType')
-        startDate = request.form.get('startDate')
-        endDate = request.form.get('endDate')
-        guestAmount = request.form.get('guestAmount')
-        price = request.form.get('totalCost')
-        transactionDate = request.form.get('transactionDate')
+    if request.method == "POST":
+        roomType = request.form.get("roomType")
+        startDate = request.form.get("startDate")
+        endDate = request.form.get("endDate")
+        guestAmount = request.form.get("guestAmount")
+        price = request.form.get("totalCost")
+        transactionDate = request.form.get("transactionDate")
 
         # Generate random booking reference
-        bookingReference = base64.urlsafe_b64encode(
-            os.urandom(6)).decode('ascii').upper()
+        bookingReference = "".join(
+            "".join(
+                random.choice(string.ascii_uppercase + string.digits) for _ in range(8)
+            )
+        ).upper()
 
-        currentHotel = db.session.query(
-            Hotel).filter(Hotel.city == city).first()
+        currentHotel = db.session.query(Hotel).filter(Hotel.city == city).first()
         userId = current_user.id
 
-        new_booking = Booking(room_type=roomType, start_date=startDate, end_date=endDate, guests=guestAmount, hotel_id=currentHotel.id,
-                              user_id=userId, price_pn=price, transaction_date=transactionDate, booking_reference=bookingReference)
+        new_booking = Booking(
+            room_type=roomType,
+            start_date=startDate,
+            end_date=endDate,
+            guests=guestAmount,
+            hotel_id=currentHotel.id,
+            user_id=userId,
+            price_pn=price,
+            transaction_date=transactionDate,
+            booking_reference=bookingReference,
+        )
 
         # Add booking data to DB session
         db.session.add(new_booking)
         db.session.commit()
 
-        return url_for('auth.successBooking', bookingId=new_booking.id)
+        return url_for("auth.successBooking", bookingId=new_booking.id)
 
-    return render_template('booking.html', hotel=hotel)
+    return render_template("booking.html", hotel=hotel)
 
 
 @auth.route("/bookingSuccess/<bookingId>")
@@ -151,14 +191,13 @@ def booking(city):
 def successBooking(bookingId):
     # Get booking details from ID.
     booking = Booking.query.filter_by(id=int(bookingId)).first()
-    if(booking.user_id != current_user.id):
+    if booking.user_id != current_user.id:
         # Abort if logged in user is mot the user assigned to the booking.
         abort(403)
     # Join Hotel object from Booking.
-    hotel = db.session.query(Hotel).filter(
-        Hotel.id == booking.hotel_id).first()
+    hotel = db.session.query(Hotel).filter(Hotel.id == booking.hotel_id).first()
 
-    return render_template('bookingSuccess.html', booking=booking, hotel=hotel)
+    return render_template("bookingSuccess.html", booking=booking, hotel=hotel)
 
 
 @auth.route("/getInvoice/<bookingId>")
@@ -166,15 +205,13 @@ def successBooking(bookingId):
 def getInvoice(bookingId):
     # Get booking details from ID.
     booking = Booking.query.filter_by(id=int(bookingId)).first()
-    if(booking.user_id != current_user.id):
+    if booking.user_id != current_user.id:
         # Abort if logged in user is mot the user assigned to the booking.
         abort(403)
     # Join Hotel object from Booking.
-    hotel = db.session.query(Hotel).filter(
-        Hotel.id == booking.hotel_id).first()
+    hotel = db.session.query(Hotel).filter(Hotel.id == booking.hotel_id).first()
     receipt = Receipt()
     pdf = receipt.GeneratePDF(booking=booking, hotel=hotel)
-    path = current_app.config['CLIENT_PDF'] + \
-        booking.booking_reference + '-booking.pdf'
-    pdf.output(path, dest='S')
+    path = current_app.config["CLIENT_PDF"] + booking.booking_reference + "-booking.pdf"
+    pdf.output(path, dest="S")
     return send_file(path, environ=request.environ, as_attachment=True)
--- app/pdf.py
+++ app/pdf.py
@@ -1,4 +1,5 @@
 from fpdf import FPDF, FlexTemplate
+import qrcode
 
 
 class Receipt:
@@ -8,18 +9,56 @@ class Receipt:
     def GeneratePDF(self, booking=None, hotel=None):
         # this will define the ELEMENTS that will compose the template.
         elements = [
-            {'name': 'company_name', 'type': 'T', 'x1': 17.0, 'y1': 32.5, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0,
-                'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
-            {'name': 'barcode', 'type': 'C39', 'x1': 20.0, 'y1': 246.5, 'x2': 140.0, 'y2': 254.0, 'font': 'Code 39', 'size': 0.75, 'bold': 0,
-                'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': booking.booking_reference, 'priority': 3, },
+            {
+                "name": "company_name",
+                "type": "T",
+                "x1": 17.0,
+                "y1": 32.5,
+                "x2": 115.0,
+                "y2": 37.5,
+                "font": "Arial",
+                "size": 12.0,
+                "bold": 1,
+                "italic": 0,
+                "underline": 0,
+                "foreground": 0,
+                "background": 0,
+                "align": "I",
+                "text": "",
+                "priority": 2,
+            },
+            {
+                "name": "barcode",
+                "type": "C39",
+                "x1": 20.0,
+                "y1": 246.5,
+                "x2": 140.0,
+                "y2": 254.0,
+                "font": "Code 39",
+                "size": 0.75,
+                "bold": 0,
+                "italic": 0,
+                "underline": 0,
+                "foreground": 0,
+                "background": 0,
+                "align": "I",
+                "text": booking.booking_reference,
+                "priority": 3,
+            },
         ]
 
-        pdf = FPDF()
+        pdf = FPDF(format="A4", orientation="portrait")
+        pdf.set_font("Arial", "B", 20)
         pdf.add_page()
+        pdf.cell(30, 10, "Horizon Hotels")
+        pdf.cell(100)
+        img = qrcode.make(booking.booking_reference)
+        pdf.image(img.get_image(), x=150, y=0, w=50, h=50)
+        pdf.ln(20)
+        pdf.set_font("Arial", size=12)
 
         # here we instantiate the template and define the HEADER
         tpl = FlexTemplate(pdf, elements=elements)
-        tpl["company_name"] = "Horizon Hotels"
         tpl.render()
 
         return pdf
