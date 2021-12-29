9ca07d619737704c497ed5323e44809ad89bccd5
Jago Gardiner <jagogardiner@gmail.com>
2021-12-29 23:11:08 +0000

    Adding accounts url, fixing active button bug
    Change-Id: Ia7f83476f9128d752124c8e202e7193a728d70dd
    Signed-off-by: Jago Gardiner <jagogardiner@gmail.com>

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -141,3 +141,9 @@ def logout():
 def booking(city):
     hotel = Hotel.query.filter_by(city=city).first_or_404()
     return render_template("booking.html", hotel=hotel)
+
+
+@auth.route("/account")
+@login_required
+def account():
+    return render_template("account.html")
